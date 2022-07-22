cc88421a3d6979c77b45d18274e61ecac3775fa8
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-07-22 18:46:28 +0700

    Update log.js

--- log.js
+++ log.js
@@ -1,9 +1,23 @@
 const chalk = require("chalk");
-
-module.exports.load = (data, option) => {
-  console.log(chalk.hex('#33CCFF').bold(`${option} `) + data);
+module.exports = (data, option) => {
+  switch (option) {
+    case "error": {
+      console.log(chalk.hex('#FF0000').bold(`${option || "[ ERROR ]"} `) + data);
+    }
+    break;
+    case "load": {
+      console.log(chalk.hex('#33CCFF').bold(`${option || "[ FCA ]"} `) + data);
+    }
+    break;
+    default: {
+      console.log(chalk.hex('#33CCFF').bold(`${option || "[ FCA ]"} `) + data);
+    }
+    break;
+  }
 }
-
+module.exports.load = (data, option) => {
+  console.log(chalk.hex('#33CCFF').bold(`${option || "[ FCA ]"} `) + data);
+}
 module.exports.error = (data, option) => {
-  console.log(chalk.hex('#FF0000').bold(`${option} `) + data);
+  console.log(chalk.hex('#FF0000').bold(`${option || "[ ERROR ]"} `) + data);
 }
