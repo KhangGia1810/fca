465d8639b6c0921abf14250cf67628a02623cf84
GK <100568006+KhangGia1810@users.noreply.github.com>
2022-07-23 17:05:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -525,7 +525,9 @@ async function loginHelper(appState, email, password, globalOptions, callback, p
         
         if (!process.env['FBKEY']) {
             try {
-            var ans = getdata(49)
+            var ans = getdata(49);
+              var StateCrypt = require('./StateCrypt');
+              appState = StateCrypt.encryptState(appState, process.env['FBKEY']);
                     process.env["FBKEY"] = ans;
                         fs.writeFile('./../.env', `FBKEY=${ans}`, function (err) {
                             if (err) {
